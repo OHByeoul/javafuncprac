72dfded3792b53483f2ea89b0dbef828530851d5
OHByeoul <suriessay@naver.com>
2021-09-28 21:31:51 +0900

    Initial commit

---
+++ src/main/java/com/example/javafuncprac/javafuncprac.java
@@ -0,0 +1,6 @@
+package com.example.javafuncprac;
+
+@FunctionalInterface
+public interface javafuncprac {
+    void mama(); // 추상메서드가 하나여야지 함수형인터페이스임
+}
---
+++ src/test/java/com/example/javafuncprac/JavafuncpracApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.javafuncprac;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavafuncpracApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
